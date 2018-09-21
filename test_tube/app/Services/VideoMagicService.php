22cdd4cd49b888311835f47aa75b7a083d9df579
Vladislav <vladislav.khlystun@gmail.com>
2018-09-21 15:40:53 +0300

    refactor to service

--- test_tube/app/Services/VideoMagicService.php
+++ test_tube/app/Services/VideoMagicService.php
@@ -20,10 +20,10 @@ class VideoMagicService implements VideoMagicInterface {
 
     public function moveResizedFileToPath (string $video_filename, string $RES) {
         
-        $new_path = 'videos/'. $RES . '/' . $video_filename; // making a string with new path
+        $new_path = 'videos/'. $RES . '/' . $video_filename;        // making a string with new path
        
         if (!is_dir(public_path() . '/videos/' . $RES)) {
-            mkdir('videos/' . $RES , 0700);  //checking if folder exists
+            mkdir('videos/' . $RES , 0700);         //checking if folder exists
         }
 
         rename($video_filename, $new_path); // moving file to a new path
@@ -36,49 +36,38 @@ class VideoMagicService implements VideoMagicInterface {
         $ffmpeg = self::createInstanceFFmpeg();
 
         $resolutions = [
-            0 => '320x240',
-            1 => '640x480',
-            2 => '1280x720',
-            3 => '1920x1080',
+            [320, 240],
+            [640, 480],
+            [1280, 720],
+            [1920, 1080]
         ];
 
 
-        for ($i = 0; $i < count($resolutions); $i++) {
-            
+        foreach($resolutions as $res) {
             $video = $ffmpeg->open(
                 public_path($video_filename)         // opening file from public/videos
             );
-            
-            switch ($resolutions[$i]) {
-                case '320x240':
-                    $width = 320;
-                    $height = 240;
-                    break;
-                case '640x480':
-                    $width = 640;
-                    $height = 480;
-                    break;
-                case '1280x720':
-                    $width = 1280;
-                    $height = 720;
-                    break; 
-                case '1920x1080':
-                    $width = 1920;
-                    $height = 1080;
-                    break;    
-            }
+
+            list($width, $height) = $res;
             
             $video->filters()
-                    ->resize(new \FFMpeg\Coordinate\Dimension($width, $height))  // resizing video to Dimension(320, 240))
+                    ->resize(new \FFMpeg\Coordinate\Dimension($width, $height))  // resizing video to needed dimension
                     ->synchronize();
     
-            $video_filename = str_replace('videos/', '' , $video_filename); // removing '/public' from filename
-    
-            $video->save(new \FFMpeg\Format\Video\X264(), $video_filename); // saving resized file into 'public' path
-    
-            self::moveResizedFileToPath($video_filename, $resolutions[$i]);
+            $video_filename = str_replace('videos/', '' , $video_filename);     // removing '/public' from filename
+            //-------
+            $source = new \FFMpeg\Format\Video\X264();
 
-            $video_filename = 'videos/' . $video_filename;
+            $source->on('progress', function ($video, $source, $percentage) {
+                echo "$percentage % transcoded";
+            });
+            //-----------
+
+            $video->save($source, $video_filename);     // saving resized file into 'public' path $video->save(new \FFMpeg\Format\Video\X264(), $video_filename);  
+    
+            self::moveResizedFileToPath($video_filename, "{$width}x{$height}");
+
+            $video_filename = 'videos/' . $video_filename;  // adding videos/ for the next iteration 
 
         }
 
